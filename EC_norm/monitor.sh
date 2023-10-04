713fe12215b684f6512c7574913b65acde588907
drondistortion <cancelbutton@gmail.com>
2023-10-04 18:24:00 +0300

    clean up

--- EC_norm/monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PORT=/dev/ttyUSB0
-if [[ $# -gt 0 ]]
-then PORT=$1
-fi
-echo $PORT
-arduino-cli monitor -p $PORT -c 115200,8,1
--- pH_norm/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-arduino-cli compile -b esp32:esp32:piranha_esp-32 -e -v --build-property "build.partitions=default" --build-property "build.code_debug=5"
